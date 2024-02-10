f8c7780569b107c4b012fd774dbc12b59625903b
seiya_tagami <bepozurapu274@gmail.com>
2024-02-10 12:44:54 +0900

    feat: ddl.sqlを追加

--- service/docker/db/script/ddl.sql
+++ service/docker/db/script/ddl.sql
@@ -1,1 +1,25 @@
 -- コンテナ初期起動時に流すスクリプトを記述する
+drop database if exists peco_db;
+create database peco_db;
+use peco_db;
+
+-- ユーザーテーブル
+drop table if exists users;
+create table users (
+  id int not null primary key,
+  name varchar(256) not null,
+  email varchar(256) not null
+) charset=utf8;
+
+-- レストランテーブル
+drop table if exists restaurants;
+create table restaurants (
+  id varchar(256) not null primary key,
+  name varchar(256) not null,
+  genre varchar(256) not null,
+  nearest_station varchar(256) not null,
+  address varchar(256) not null,
+  url varchar(256) not null,
+  user_id int not null,
+  foreign key (user_id) references users (id)
+) charset=utf8;
