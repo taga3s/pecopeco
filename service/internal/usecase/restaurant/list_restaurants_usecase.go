637f4a3a4b5b45bbecdaf7fef7c93b30ddc15fc1
seiya_tagami <bepozurapu274@gmail.com>
2024-02-12 09:20:42 +0900

    feat: listのusecaseを追加

---
+++ service/internal/usecase/restaurant/list_restaurants_usecase.go
@@ -0,0 +1,51 @@
+package restaurant
+
+import (
+	"context"
+
+	restaurantDomain "github.com/Seiya-Tagami/pecopeco-service/internal/domain/restaurant"
+)
+
+type ListRestaurantsUseCase struct {
+	restaurantRepo restaurantDomain.RestaurantRepository
+}
+
+func NewListRestaurantsUseCase(
+	restaurantRepo restaurantDomain.RestaurantRepository,
+) *ListRestaurantsUseCase {
+	return &ListRestaurantsUseCase{
+		restaurantRepo: restaurantRepo,
+	}
+}
+
+type ListRestaurantsUseCaseDto struct {
+	ID             string
+	Name           string
+	Genre          string
+	NearestStation string
+	Address        string
+	URL            string
+	UserID         string
+}
+
+func (uc *ListRestaurantsUseCase) Run(ctx context.Context, userID string) ([]*ListRestaurantsUseCaseDto, error) {
+	restaurants, err := uc.restaurantRepo.ListByUserID(ctx, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	dtos := make([]*ListRestaurantsUseCaseDto, 0, len(restaurants))
+
+	for _, v := range restaurants {
+		dtos = append(dtos, &ListRestaurantsUseCaseDto{
+			ID:             v.ID,
+			Name:           v.Name,
+			Genre:          v.Genre,
+			NearestStation: v.NearestStation,
+			Address:        v.Address,
+			URL:            v.URL,
+			UserID:         v.UserID,
+		})
+	}
+	return dtos, nil
+}
